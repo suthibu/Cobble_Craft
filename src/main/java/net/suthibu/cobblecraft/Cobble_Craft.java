82b4eb68542942cf394d2d8a522c331f077a0b72
スティーブ <suthibu28@gmail.com>
2024-10-13 22:54:37 +0900

    a

--- src/main/java/net/suthibu/cobblecraft/Cobble_Craft.java
+++ src/main/java/net/suthibu/cobblecraft/Cobble_Craft.java
@@ -1,5 +1,7 @@
 package net.suthibu.cobblecraft;
 
+import net.minecraft.world.item.CreativeModeTabs;
+import net.suthibu.cobblecraft.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -34,6 +36,8 @@ public class Cobble_Craft {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
@@ -46,7 +50,9 @@ public class Cobble_Craft {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.COBBLESTONE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/suthibu/cobblecraft/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.suthibu.cobblecraft.item;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.suthibu.cobblecraft.Cobble_Craft;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Cobble_Craft.MOD_ID);
+
+    public static final DeferredItem<Item> COBBLESTONE = ITEMS.register("cobblestone",
+            () -> new Item(new Item.Properties()));
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
