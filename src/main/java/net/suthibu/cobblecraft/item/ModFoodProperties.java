97237ac302b0f0f01006cfc404bd9170f0a2b873
スティーブ <suthibu28@gmail.com>
2024-11-03 13:39:59 +0900

    バカ飯

--- src/main/java/net/suthibu/cobblecraft/item/ModFoodProperties.java
+++ src/main/java/net/suthibu/cobblecraft/item/ModFoodProperties.java
@@ -9,6 +9,16 @@ public class ModFoodProperties {
             .nutrition(2100000000)
             .fast()
             .alwaysEdible()
-            .effect(() -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 100000), 5000f)
-            .effect(() -> new MobEffectInstance(MobEffects.HEALTH_BOOST, 1000000),5000f).build();
+            .effect(() -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 100000,1000),1f)
+            .effect(() -> new MobEffectInstance(MobEffects.DAMAGE_BOOST, 1000000,2100000000),1f)
+            .effect(() -> new MobEffectInstance(MobEffects.CONDUIT_POWER,1000000,1000),1f)
+            .effect(() -> new MobEffectInstance(MobEffects.DIG_SPEED,1000000,255),1f)
+            .effect(() -> new MobEffectInstance(MobEffects.DOLPHINS_GRACE,100000,100000),1f)
+            .effect(() -> new MobEffectInstance(MobEffects.ABSORPTION,1000000,1000000),1f)
+            .effect(() -> new MobEffectInstance(MobEffects.HEAL,1000000,10000000),1f)
+            .effect(() -> new MobEffectInstance(MobEffects.SATURATION,10000000,100000000),1f)
+            .effect(() -> new MobEffectInstance(MobEffects.ABSORPTION,1000000,1000000),1f)
+            .effect(() -> new MobEffectInstance(MobEffects.HEALTH_BOOST, 1000000,1000),1f).build();
+
+
 }
